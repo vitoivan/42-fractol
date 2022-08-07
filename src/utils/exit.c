63568f9ae4571df736edf446ca5d429ba0882850
Victor Ivan <victor.simoes@brmalls.com.br>
2022-08-07 14:38:21 -0300

    adding mlx_destroy_window

--- src/utils/exit.c
+++ src/utils/exit.c
@@ -6,7 +6,7 @@
 /*   By: vivan-de <vivan-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 09:11:18 by vivan-de          #+#    #+#             */
-/*   Updated: 2022/08/07 11:37:59 by vivan-de         ###   ########.fr       */
+/*   Updated: 2022/08/07 14:36:44 by vivan-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,11 @@ static void	clear_ctx(t_ctx *ctx)
 	}
 	if (ctx->window)
 		mlx_destroy_window(ctx->mlx, ctx->window);
-	free_if_exist(ctx->mlx);
+	if (ctx->mlx)
+	{
+		mlx_destroy_display(ctx->mlx);
+		free_if_exist(ctx->mlx);
+	}
 }
 
 int	exit_program(t_ctx *ctx)
